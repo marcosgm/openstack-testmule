f022db3bd6bb24d3b6efdbf784b9c5485e36416d
root <root@ks4010720.ip-192-99-10.net>
2013-12-04 14:06:24 -0500

    cleaned up, all in single file to ensure puppet apply

--- README.md
+++ README.md
@@ -9,14 +9,22 @@ Have a fully virtual environment to test all of Openstack features, specially wi
  As only one IPv4 is provided by OVH, but many IPv6 are available, we will probably have to use NAT for IPv4 or go fully IPv6.
 
 REQUIRES: 
--Fedora 19 64 bits
+-Centos 6.4  64 bits
 -Regular kernel with KVM (don't use OVH one)
 -- do 'yum install kernel; grub2-mkconfig -o /boot/grub2/grub.cfg' and reboot
 -- or, you can follow this guide to install the default kernel 
 
+FIRST STEPS
+yum update
+rpm -ivh http://fedora.mirror.nexicom.net/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install puppet git
+cd && git clone git@github.com:marcosgm/openstack-testmule.git
+cd openstack-testmule/manifests
+puppet apply node-testmule.pp
+
 PUPPET REQUIREMENTS:
-(puppet 3 installed via 'yum install puppet')
-puppet module install carlasouza/virt
+puppet module install puppetlabs-apache
+puppet module install spiette/selinux
 puppet module install spiette/selinux
 puppet module install thias/libvirt
 puppet module install puppetlabs-stdlib
--- manifests/node-testmule.pp
+++ manifests/node-testmule.pp
@@ -1,11 +1,116 @@
 node default {
-    file { "/tmp/test":
-        owner => 'root',
-        group => 'root',
-        mode  => '0440',
-	content => 'this is only a test\n',
-    }
     include hypervisor
     include webproxy
 }
 
+
+class webproxy {
+    class { 'apache':  
+              default_mods => true,
+              default_vhost => false,
+    }
+  include apache::mod::proxy                                                                                                                                                                     
+
+  apache::vhost { 'vm-foreman.mooo.com':
+      port    => '443',
+      ssl     => true,
+      docroot => '/var/www/html',
+      proxy_pass => [
+         { path => '/', url => 'https://192.168.1.2' },
+      ]
+  }
+}
+
+class hypervisor{
+	$basicpackages = ['vim-enhanced','puppet','git','screen','mlocate','lsof','etckeeper']
+	
+	package { $basicpackages: ensure => installed }
+
+	$hypervisorpackages = ['bridge-utils','kernel','qemu-kvm-tools','virt-viewer'] #libvirt, qemu-kvm not necessary
+	
+	package { $hypervisorpackages: ensure => installed }
+	package { ['virt-manager','redhat-lsb','xorg-x11-xauth','dejavu-lgc-sans-fonts']: ensure => installed }
+
+
+	#SELINUX
+	class { 'selinux':
+	  mode => 'disabled'
+	}
+	include hypervisor::libvirtconf
+	include hypervisor::firewall
+	include hypervisor::network
+	include hypervisor::polipo
+}
+
+class hypervisor::polipo{
+	package {'polipo':
+		ensure => installed
+	}
+	service {'polipo':
+                ensure => running,
+        #        enable => true,
+	}
+}
+
+class hypervisor::firewall{
+	package {"system-config-firewall-tui": ensure=> installed }
+	service {"iptables": 
+		#enable => true,
+		ensure => running,
+	}
+}
+
+class hypervisor::libvirtconf{
+
+	#fix for #Error: internal error Process exited while reading console log output: chardev: opening backend "pty" failed: Permission denied
+	user {'qemu':
+		groups => ['qemu','kvm','tty'] , #tty group
+	}
+
+	class { 'libvirt':
+	  defaultnetwork => false, # This is the default
+	}
+
+	libvirt::network { 'mule-ext':
+   		ensure             => 'enabled',
+   		autostart          => true,
+   		forward_mode       => 'bridge',
+   		forward_dev        => 'muleNIC0',
+	}	
+	libvirt::network { 'mule-int':
+   		ensure             => 'enabled',
+   		autostart          => true,
+   		forward_mode       => 'bridge',
+   		forward_dev        => 'muleNIC1',
+	}
+}
+
+class hypervisor::network {
+
+        network::bridge::static { 'mule-ext':
+	  ensure    => 'up',
+	  ipaddress => '192.168.1.1',
+	  netmask   => '255.255.255.0',
+	}
+	network::bridge::static { 'mule-int':
+	  ensure    => 'up',
+	  ipaddress => '172.20.1.1',
+	  netmask   => '255.255.255.0',
+	}
+	
+	file {'/etc/modprobe.d/dummy.conf':
+		content=>"	alias dummy0 muleNIC0
+			alias dummy1 muleNIC1
+			options dummy numdummies=2 \n",
+		mode=>664,
+	}
+	#a reboot is needed to create the dummy interfaces
+	network::if::bridge { 'muleNIC0':
+	  ensure => 'up',
+	  bridge => 'mule-ext'
+	}
+	network::if::bridge { 'muleNIC1':
+	  ensure => 'up',
+	  bridge => 'mule-int'
+	}
+}
